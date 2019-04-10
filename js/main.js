64aaeb1cf868ec83f2f780c419e52ec94eafaca2
bsmith20681 <bsmith20681@gmail.com>
2019-04-09 18:42:53 -0600

    added pictures of super heros

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-const characterId = 'https://gateway.marvel.com/v1/public/characters?name=Thor&apikey=bf63e86f55b219bcd579082f0eed4d21'
+const characterId = 'https://gateway.marvel.com/v1/public/characters?'
+const apiKey = '&apikey=bf63e86f55b219bcd579082f0eed4d21'
+
+const thor = 'name=Thor'
+
 const name = document.querySelector('.name')
 const description = document.querySelector('.description')
 
 
-  fetch(characterId)
+  fetch(characterId + thor)
     .then(response => response.json())
     .then(data => {
       console.log(data.data)
@@ -11,3 +15,14 @@ const description = document.querySelector('.description')
       name.innerHTML = data.data.results[0].name
       description.innerHTML = data.data.results[0].description
     })
+
+
+
+
+
+//Thor 1009664
+//Hulk 1009351
+//Spider-Man 1009610
+//Captain America 1009220
+//Black Widow 1009189
+//Thanos 1009652
