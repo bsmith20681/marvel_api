5a2307a8a26c78cc5dd80d430f0781a3e4afafe8
bsmith20681 <bsmith20681@gmail.com>
2019-04-13 21:00:38 -0600

    added new api for comics

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const characterId = 'https://gateway.marvel.com/v1/public/characters?'
-const apiKey = '&apikey=bf63e86f55b219bcd579082f0eed4d21'
+const characterComic = 'https://gateway.marvel.com/v1/public/characters/'
+const apiKey = 'apikey=bf63e86f55b219bcd579082f0eed4d21'
+
+
+//hero's id numbers
+const thorId = '1009664'
 
 
 const heroName = document.querySelector('.heroName')
@@ -8,7 +13,7 @@ const heroComic = document.querySelector('.heroComic')
 
 function thor(){
   const thor = 'name=Thor'
-  fetch(characterId + thor + apiKey)
+  fetch(characterId + thor + '&' + apiKey)
     .then(response => response.json())
     .then(data => {
       console.log(data.data)
@@ -17,6 +22,12 @@ function thor(){
       heroDescription.innerHTML = data.data.results[0].description
       //heroComic.innerHTML = data.data.results[0].
     });
+
+  fetch(characterComic + thorId + '/comics?' + apiKey)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.data)
+    })
 }
 
 
