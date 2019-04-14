e1e713465d3a8af049fb1379e047e70176e181e4
bsmith20681 <bsmith20681@gmail.com>
2019-04-14 09:31:30 -0600

    added comic book covers via api

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ const thorId = '1009664'
 const heroName = document.querySelector('.heroName')
 const heroDescription = document.querySelector('.heroDescription')
 const heroComic = document.querySelector('.heroComic')
+const comicCover1 = document.querySelector('.comicCover1')
+const comicCover2 = document.querySelector('.comicCover2')
+const comicCover3 = document.querySelector('.comicCover3')
+
 
 function thor(){
   const thor = 'name=Thor'
@@ -27,6 +31,10 @@ function thor(){
     .then(response => response.json())
     .then(data => {
       console.log(data.data)
+      comicCover1.src = data.data.results[0].images[0].path + "/portrait_incredible.jpg"
+      comicCover2.src = data.data.results[1].images[0].path + "/portrait_incredible.jpg"
+      comicCover3.src = data.data.results[2].images[0].path + "/portrait_incredible.jpg"
+
     })
 }
 
